38688589333560df39469998ecbe82b80222c490
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-08-27 01:05:24 +0200

    Add computing presence, minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import WebUntisType, { Absences, Lesson } from "webuntis";
+import WebUntisType, { Absences, Lesson, ShortData } from "webuntis";
 const WebUntis = require("webuntis");
 require("dotenv/config");
 import { writeFile, readFile } from "fs/promises";
 import { join } from "path";
 
-const globalUntis = new WebUntis(
-    process.env.SCHOOL,
-    process.env.USERNAME,
-    process.env.PASSWORD,
-    process.env.SERVER_URL
-) as WebUntisType;
+interface Credentials {
+    school: string;
+    username: string;
+    password: string;
+    serverUrl: string;
+}
+interface SchoolYear {
+    startDate: Date;
+    endDate: Date;
+}
 
-const currentSchoolYear = {
-    name: "2022/2023",
-    id: 19,
-    startDate: new Date("2022-08-21T22:00:00.000Z"),
-    endDate: new Date("2023-07-11T22:00:00.000Z"),
+const getSubjectId = (subjects: ShortData[]) => {
+    return subjects.map((su) => `${su.longname} (${su.name})`).join(", ");
 };
 
 const getCurrentSchoolYear = (untis: WebUntisType) => {
@@ -29,10 +30,13 @@ const getLessonsForToday = async (untis: WebUntisType) => {
     return timetable;
 };
 
-const getTimetableForYear = async () => {
-    const lessons = await globalUntis.getOwnClassTimetableForRange(
-        currentSchoolYear.startDate,
-        currentSchoolYear.endDate
+const getLessonsForSchoolYear = async (
+    untis: WebUntisType,
+    schoolYear: SchoolYear
+) => {
+    const lessons = await untis.getOwnClassTimetableForRange(
+        schoolYear.startDate,
+        schoolYear.endDate
     );
 
     writeFile(
@@ -43,73 +47,142 @@ const getTimetableForYear = async () => {
     return lessons;
 };
 
-const getAbsences = (untis: WebUntisType) => {
-    return untis.getAbsentLesson(
-        currentSchoolYear.startDate as any,
-        currentSchoolYear.endDate as any
-    );
+const reduceSubjects = (lessons: Lesson[]) => {
+    const subjects = new Map<string, Omit<SubjectData, "presence">>();
 
-    const exampleResponse = {
-        absences: [],
-        absenceReasons: [],
-        excuseStatuses: null,
-        showAbsenceReasonChange: false,
-        showCreateAbsence: false,
-    };
+    lessons.forEach((lesson) => {
+        if (lesson.code == "irregular") return;
+        const subjectId = getSubjectId(lesson.su);
+
+        const subjectFromMap = subjects.get(subjectId);
+        subjects.set(subjectId, {
+            lessonsTotal: (subjectFromMap?.lessonsTotal || 0) + 1,
+            lessonsCancelled:
+                (subjectFromMap?.lessonsCancelled || 0) +
+                (lesson.code == "cancelled" ? 1 : 0),
+            lessonsMissed: null,
+        });
+    });
+
+    return subjects;
 };
 
-// globalUntis.login().then(async () => {
-//     getCurrentSchoolYear(globalUntis);
+interface SubjectData {
+    lessonsTotal: number;
+    lessonsCancelled: number;
+    lessonsMissed: null | number;
+    presence: number;
+}
 
-//     const absences = await getAbsences(globalUntis);
-//     // absences.absences.forEach(a => a.)
-//     console.log(absences);
+const getAbsences = async (
+    untis: WebUntisType,
+    schoolYear: SchoolYear,
+    allYearsLessons: Lesson[]
+) => {
+    // @TODO: this should only fetch absences of the current Semester
+    const untisAbsences = await untis.getAbsentLesson(
+        schoolYear.startDate as any,
+        schoolYear.endDate as any
+    );
+    const lessonsTakenPlace = allYearsLessons.filter(
+        (l) => l.code != "cancelled" && l.code != "irregular"
+    );
 
-//     const subjects = await globalUntis.getSubjects(); // if this is neede, we should cashe that
-//     // subjects.forEach(s => s.)
-//     console.log(subjects);
+    const subjectLessonsMap = reduceSubjects(allYearsLessons);
 
-//     const departments = await globalUntis.getDepartments();
-//     // departments.forEach(d => d.)
-//     console.log(departments);
+    const lessonsMissed = untisAbsences.absences
+        .map((absence) => {
+            const lessonsMissed = lessonsTakenPlace
+                .filter((lesson) => {
+                    const isLessonOnAbsenceDay =
+                        lesson.date >= absence.startDate &&
+                        lesson.date <= absence.endDate;
 
-//     globalUntis.logout();
-// });
+                    // @TODO: clarify the school rules (wether being late counts to the 70% rule)
+                    const isLessonWithinAbsenceTime =
+                        lesson.startTime <= absence.startTime &&
+                        lesson.endTime >= absence.endTime;
 
-// @TODO: make this automatically fetch the new data once a day (only fetch new data, when response is one day old)
+                    return isLessonOnAbsenceDay && isLessonWithinAbsenceTime;
+                })
+                .map(({ su }) => ({ subjectId: getSubjectId(su) }));
+
+            return lessonsMissed;
+        })
+        .flat(1);
+
+    lessonsMissed.forEach(({ subjectId }) => {
+        const subjectStats = subjectLessonsMap.get(subjectId)!;
+
+        subjectLessonsMap.set(subjectId, {
+            ...subjectStats,
+            lessonsMissed: (subjectStats.lessonsMissed || 0) + 1,
+        });
+    });
+
+    return [...subjectLessonsMap.entries()];
+};
+
+const loginUntis = async (
+    { school, username, password, serverUrl }: Credentials,
+    callback: (untis: WebUntisType) => Promise<any>
+) => {
+    const untis = new WebUntis(
+        school,
+        username,
+        password,
+        serverUrl
+    ) as WebUntisType;
+    await untis.login();
+    await callback(untis);
+    await untis.logout();
+};
+
 readFile(join(__dirname, "../responses/lessons.json")).then(async (file) => {
     const contents = file.toString();
     const lessons: Lesson[] = JSON.parse(contents);
 
-    const subjectIds = new Set<number>();
-    const subjects = new Map<
-        number,
-        { name: string; total: number; cancelled: number }
-    >();
+    const currentSchoolYear = {
+        name: "2022/2023",
+        id: 19,
+        startDate: new Date("2022-08-21T22:00:00.000Z"),
+        endDate: new Date("2023-07-11T22:00:00.000Z"),
+    };
 
-    console.log({
-        "lessons total": lessons.length,
-        "lessons irregular": lessons.filter((l) => l.code == "irregular")
-            .length,
-        "lessons cancelled": lessons.filter((l) => l.code == "cancelled")
-            .length,
-    });
+    loginUntis(
+        {
+            school: process.env.SCHOOL!,
+            username: process.env.USERNAME!,
+            password: process.env.PASSWORD!,
+            serverUrl: process.env.SERVER_URL!,
+        },
+        async (untis) => {
+            // getCurrentSchoolYear(untis);
+            // getLessonsForSchoolYear(untis)
 
-    lessons.forEach((lesson) => {
-        if (lesson.code == "irregular") return;
+            const absences = await getAbsences(
+                untis,
+                currentSchoolYear,
+                lessons
+            );
 
-        lesson.su.forEach((su) => {
-            subjectIds.add(su.id);
-            const subjectFromMap = subjects.get(su.id);
-            subjects.set(su.id, {
-                name: `${su.longname} (${su.name})`,
-                total: (subjectFromMap?.total || 0) + 1,
-                cancelled:
-                    (subjectFromMap?.cancelled || 0) +
-                    (lesson.code == "cancelled" ? 1 : 0),
+            const subjects = absences.map<SubjectData>(([subjectId, absence]) => {
+                const actualLessons =
+                    absence.lessonsTotal - absence.lessonsCancelled;
+                const presenceInPercent = (
+                    ((actualLessons - (absence.lessonsMissed || 0)) /
+                        actualLessons) *
+                    100
+                ).toFixed(2);
+
+                return {
+                    ...absence,
+                    subjectId,
+                    presence: parseFloat(presenceInPercent),
+                    lessonsMissed: absence.lessonsMissed || 0,
+                };
             });
-        });
-    });
-    console.log("subject count:", subjectIds.size);
-    console.log("subjects:", subjects);
+            console.log("Absences:", subjects);
+        }
+    );
 });
